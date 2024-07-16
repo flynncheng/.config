f704fba4b55a741699a4cc6dfe51da3722f8e766
Flynn Cheng <flynncheng.ca@gmail.com>
2024-07-15 22:01:34 -0700

    feat(fish): add starship

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# brew
 set -gx PATH /opt/homebrew/bin $PATH
 
+# starship
+starship init fish | source
+
+# nvim
 alias vim='nvim'
