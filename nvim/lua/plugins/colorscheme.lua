c11203e846b6c414d66599cb235be794ec825301
Flynn Cheng <flynncheng.ca@gmail.com>
2024-10-16 21:53:00 -0700

    style(nvim): remove transparent backgournd

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,5 @@
 return {
-  {
-    "sainnhe/everforest",
-    config = function()
-      vim.g.everforest_transparent_background = 1
-    end,
-  },
+  { "sainnhe/everforest" },
   {
     "LazyVim/LazyVim",
     opts = {
