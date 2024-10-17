a3996a8c37618a88608ec52c0447eca5f40f871d
Flynn Cheng <flynncheng.ca@gmail.com>
2024-10-16 21:54:11 -0700

    style(nvim): remove filename from statusline

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -68,7 +68,7 @@ return {
       sections = {
         lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
         -- lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-        lualine_b = { "filename", "branch" },
+        lualine_b = { "branch" },
         lualine_c = {
           "%=", --[[ add your center compoentnts here in place of this comment ]]
         },
