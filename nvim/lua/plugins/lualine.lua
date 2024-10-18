9148209927c094683fcae62270f77f8c989b34a4
Flynn Cheng <flynncheng.ca@gmail.com>
2024-10-17 19:07:30 -0700

    style(nvim): set the statusline theme to auto

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,66 +1,10 @@
--- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
-
--- stylua: ignore
-local colors = {
-  bg0    = '#323d43',
-  bg1    = '#3c474d',
-  bg3    = '#505a60',
-  fg     = '#d8caac',
-  aqua   = '#87c095',
-  green  = '#a7c080',
-  orange = '#e39b7b',
-  purple = '#d39bb6',
-  red    = '#e68183',
-  grey1  = '#868d80',
-}
-
-local everforest = {
-  normal = {
-    a = { bg = colors.green, fg = colors.bg0, gui = "bold" },
-    b = { bg = colors.bg3, fg = colors.fg },
-    c = { bg = colors.bg1, fg = colors.fg },
-  },
-  insert = {
-    a = { bg = colors.fg, fg = colors.bg0, gui = "bold" },
-    b = { bg = colors.bg3, fg = colors.fg },
-    c = { bg = colors.bg1, fg = colors.fg },
-  },
-  visual = {
-    a = { bg = colors.red, fg = colors.bg0, gui = "bold" },
-    b = { bg = colors.bg3, fg = colors.fg },
-    c = { bg = colors.bg1, fg = colors.fg },
-  },
-  replace = {
-    a = { bg = colors.orange, fg = colors.bg0, gui = "bold" },
-    b = { bg = colors.bg3, fg = colors.fg },
-    c = { bg = colors.bg1, fg = colors.fg },
-  },
-  command = {
-    a = { bg = colors.aqua, fg = colors.bg0, gui = "bold" },
-    b = { bg = colors.bg3, fg = colors.fg },
-    c = { bg = colors.bg1, fg = colors.fg },
-  },
-  terminal = {
-    a = { bg = colors.purple, fg = colors.bg0, gui = "bold" },
-    b = { bg = colors.bg3, fg = colors.fg },
-    c = { bg = colors.bg1, fg = colors.fg },
-  },
-  inactive = {
-    a = { bg = colors.bg1, fg = colors.grey1, gui = "bold" },
-    b = { bg = colors.bg1, fg = colors.grey1 },
-    c = { bg = colors.bg1, fg = colors.grey1 },
-  },
-}
-
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   opts = function()
     return {
       options = {
-        theme = everforest,
+        theme = "auto",
         component_separators = "",
         section_separators = { left = "", right = "" },
         -- section_separators = { left = "", right = "" },
@@ -71,6 +15,7 @@ return {
         lualine_b = { "branch" },
         lualine_c = {
           "%=", --[[ add your center compoentnts here in place of this comment ]]
+          { "filename", path = 1 },
         },
         lualine_x = {},
         lualine_y = { "filetype", "progress" },
